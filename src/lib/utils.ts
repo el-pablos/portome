58c97692b42382e7f43a6f411df79261a0aebdbd
el-pablos <yeteprem.end23juni@gmail.com>
2026-03-02 05:38:58 +0700

    add: pasang typescript + tsconfig + struktur shadcn-like biar project siap ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,11 @@
-import { clsx } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 /**
  * Fungsi utility untuk menggabungkan class names dengan Tailwind CSS
  * Menggunakan clsx untuk conditional classes dan twMerge untuk menghindari konflik
- * 
- * @param {...any} inputs - Class names yang akan digabungkan
- * @returns {string} - Class names yang sudah digabungkan dan di-merge
  */
-export function cn(...inputs) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
 
